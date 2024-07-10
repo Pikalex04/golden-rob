caf21f80b6918d58ec7a1cdef0916d8d5fdeafd2
Pikalex04 <aleboi106@gmail.com>
2024-07-10 16:19:37 +0200

    Updated Wiimmfi DNS
    Updated the DNS of Wiimmfi from the discontinued DNS to the new one.

--- grobDB/commands/dns.py
+++ grobDB/commands/dns.py
@@ -14,7 +14,7 @@ def run(bot):
                   'A DNS written in **bold** is recommended.')
         e.set_author(name='DNS List', icon_url='https://cdn.discordapp.com/emojis/1233445494030729236.png?size=256')
         for field in [
-            ['Wiimmfi', 'Primary DNS:\n`167.86.108.126`\n\n*Secondary DNS:\n**- `8.8.8.8`** or\n- `1.1.1.1`*'],
+            ['Wiimmfi', 'Primary DNS:\n`167.235.229.36`\n\n*Secondary DNS:\n**- `8.8.8.8`** or\n- `1.1.1.1`*'],
             ['WiiLink WFC', 'Primary DNS:\n`5.161.56.11`\n\n*Secondary DNS:\n**- `8.8.8.8`** or\n- `1.1.1.1`*'],
             ['Zwei.moe', 'Primary DNS:\n`172.104.88.237`\n\n*Secondary DNS:\n**- `8.8.8.8`** or\n- `1.1.1.1`*']
         ]:
